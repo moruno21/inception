dc1e241b1412973924f22793710b0cfb3cbea8f7
Antonio Moruno <amoruno21@gmail.com>
2023-10-18 19:59:46 +0200

    chore(web): configure i18next for translations

--- packages/web/index.tsx
+++ packages/web/index.tsx
@@ -1,9 +1,11 @@
+import './config/i18n'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-const App = () => <>Hello world!</>
+const App = () => <>Hello World</>
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
