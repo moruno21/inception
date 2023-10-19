6d3484abd09b05e32af76bf084b71fbc286fe15e
Antonio Moruno <amoruno21@gmail.com>
2023-10-19 17:34:22 +0200

    chore(web): configure routing

--- packages/web/index.tsx
+++ packages/web/index.tsx
@@ -2,14 +2,19 @@ import './config/i18n'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import GlobalStyle from 'shared/styles/global'
 import theme from 'shared/styles/theme'
 import { ThemeProvider } from 'styled-components'
 
+import AppRoutes from './services/routing/Routes'
+
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <>Hello World</>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   </ThemeProvider>
 )
 
