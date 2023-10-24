79f7cbc675f2ffc69cbb84a93db374a744a88921
Antonio Moruno <amoruno21@gmail.com>
2023-10-25 00:54:02 +0200

    chore(api): config module

--- packages/api/app.module.ts
+++ packages/api/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 
 import { AppController } from '~/src/app.controller'
@@ -7,6 +8,15 @@ import { AppService } from '~/src/app.service'
 @Module({
   controllers: [AppController],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        `.env.${process.env.NODE_ENV}.local`,
+        `.env.${process.env.NODE_ENV}`,
+        '.env.local',
+        '.env',
+      ],
+      isGlobal: true,
+    }),
     MongooseModule.forRootAsync({
       useFactory: () => ({ uri: process.env.MONGODB_URI }),
     }),
