753147749202036b9a7e30bcd95d835be81dd7b9
Antonio Moruno <amoruno21@gmail.com>
2023-10-25 00:51:16 +0200

    chore(api): configure mongo

--- packages/api/app.module.ts
+++ packages/api/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 
 import { AppController } from '~/src/app.controller'
 import { AppService } from '~/src/app.service'
 
 @Module({
   controllers: [AppController],
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({ uri: process.env.MONGODB_URI }),
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
